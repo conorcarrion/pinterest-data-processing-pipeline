a38302010d1e23e14bcc98ddde920aa1d38b7cc9
Conor Quinn <cjq234@gmail.com>
2023-01-21 12:33:43 +0000

    redrafting readme

--- lib/batch_processor.py
+++ lib/batch_processor.py
@@ -10,4 +10,5 @@ def run():
     spark = Spark()
     df = spark.load_batch_from_bucket()
     df = DataCleaner.remove_bad_values(df)
+    # load dataframe to HBASE or storage
     client.delete_json_files("project-pin-api")
